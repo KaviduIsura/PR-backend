be91e43e20bb0aaf7a7e11fb7778687256a0ccae
KaviduIsura <kaviduisura4567@gmail.com>
2024-12-01 11:58:55 +0530

    create databse connection📊✅

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 import bodyParser from "body-parser";
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
+
 app.use(bodyParser.json);
 
 //database connetion
+const mongoUrl = process.env.MONGODB_URI;
+mongoose.connect(mongoUrl, {});
+
+const connection = mongoose.connection;
+
+connection.once("open", () => {
+  console.log("Database connected");
+});
 
 app.listen(5000, () => {
   console.log("Server is running on port 5000");

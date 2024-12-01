e18017876504b0cd01c772cc93a8dccb1e71a2cd
KaviduIsura <kaviduisura4567@gmail.com>
2024-12-01 11:46:22 +0530

    init bodypaser ✅

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+import bodyParser from "body-parser";
 import express from "express";
 
 const app = express();
+app.use(bodyParser.json);
+
+//database connetion
 
 app.listen(5000, () => {
   console.log("Server is running on port 5000");

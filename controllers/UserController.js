abae2988cec2bfb7184e7696796874cde4e2c54b
KaviduIsura <kaviduisura4567@gmail.com>
2024-12-01 15:47:01 +0530

    convert createUser function to  async function 👍

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,23 +1,20 @@
 import User from "../models/User.js";
 import bcrypt from "bcrypt";
 
-export function createUser(req, res) {
-  const newUserData = req.body;
-  newUserData.password = bcrypt.hashSync(newUserData.password, 10);
-  const user = new User(newUserData);
-
-  user
-    .save()
-    .then(() => {
-      res.json({
-        message: "User Created",
-      });
-    })
-    .catch(() => {
-      res.json({
-        message: "User not created",
-      });
+export async function createUser(req, res) {
+  try {
+    const newUserData = req.body;
+    newUserData.password = bcrypt.hashSync(newUserData.password, 10);
+    const user = new User(newUserData);
+    await user.save();
+    res.json({
+      message: "User Created",
     });
+  } catch (error) {
+    res.json({
+      message: error.message,
+    });
+  }
 }
 
 export function getUsers(req, res) {

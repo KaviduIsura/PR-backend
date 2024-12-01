03efc75b0e55e709d97c4ec801ef6ade411bb446
KaviduIsura <kaviduisura4567@gmail.com>
2024-12-01 19:03:09 +0530

    create Booking model 📝✅

---
+++ models/Booking.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const bookingSchema = mongoose.Schema({
+  bookingId:{
+    type:String,
+    required:true,
+    unique:true,
+  }
+  userId:{
+    type:String,
+    required:true
+  },
+  roomId:{
+    type:String,
+    required:true
+  },
+  status:{
+    type:String,
+    required:true,
+    default:"Pending"
+  },
+  bookingData:{
+    type:Date,
+    required:true,
+    default:Date.now
+  },
+  paymentStatus:{
+    type:String,
+    required:true,
+    default:"Pending"
+  }
+})
+
+const Booking = mongoose.model("bookings",bookingSchema)
+export default Booking

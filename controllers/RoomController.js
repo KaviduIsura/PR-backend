ef7dde896be6831bd53b81788e58cfee47ef3820
Kavidu Isura <144512406+KaviduIsura@users.noreply.github.com>
2024-12-21 00:46:24 +0530

    update create room function with hostel id validation 🔧

--- controllers/RoomController.js
+++ controllers/RoomController.js
@@ -1,8 +1,24 @@
+import Hostel from "../models/Hostel.js";
 import Room from "../models/Room.js";
+import { isAdmin } from "./UserController.js";
 
 export async function createRoom(req, res) {
   try {
+   
+    if (!isAdmin(req)) {
+      res.status(403).json({
+        message: "Please login as an administrator to add a Room!",
+      });
+      return;
+    }
     const newRoomData = req.body;
+    const hostel = await Hostel.findOne({ hostelId: newRoomData.hostelId})
+    if(!hostel){
+      res.json({
+        message:"Hostel with id "+newRoomData.hostelId+"not found"
+      })
+      return
+    }
     const room = new Room(newRoomData);
     await room.save();
     res.json({
@@ -14,6 +30,7 @@ export async function createRoom(req, res) {
     });
   }
 }
+
 
 export async function getRooms(req, res) {
   try {

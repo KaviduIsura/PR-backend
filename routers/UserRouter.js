602d809650b0712d666aaa8c075351f08589f7fa
Kavidu Isura <144512406+KaviduIsura@users.noreply.github.com>
2024-12-20 19:44:37 +0530

    Add path to login  🔧

--- routers/UserRouter.js
+++ routers/UserRouter.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { createUser, getUsers } from "../controllers/UserController.js";
+import { createUser, getUsers, login } from "../controllers/UserController.js";
 
 const userRouter = express.Router();
 
 userRouter.post("/", createUser);
 userRouter.get("/", getUsers);
+userRouter.post("/login",login)
 
 export default userRouter;
